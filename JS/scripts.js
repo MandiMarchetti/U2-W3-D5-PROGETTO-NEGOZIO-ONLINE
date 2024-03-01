ae12452cf01c9de9006a5debc9228d396a5c6647
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-03-01 15:25:59 +0100

    update

---
+++ JS/scripts.js
@@ -0,0 +1,4 @@
+const apiKey =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWUxYWFjYTRjNTllYzAwMTk5MGQ3NTQiLCJpYXQiOjE3MDkyODgxMzgsImV4cCI6MTcxMDQ5NzczOH0.MBKMkqsPomi3xr_-AKnvOv0eHBxLjn8NEZRcQpcaULM";
+
+const URL = "https://striveschool-api.herokuapp.com/api/product/";
